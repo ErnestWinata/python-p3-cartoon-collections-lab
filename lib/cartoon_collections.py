734b469949bb6b5b3c14c65e95acd0cf7347b804
Ernest <ernestwinata@gmail.com>
2023-07-15 23:44:16 -0700

    create my commit

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,23 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(dwarves):
+    for i in range(len(dwarves)):
+        print(f"{i+1}. {dwarves[i]}")
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(planeteer_calls):
+    capitalized_calls = [call.capitalize() + '!' for call in planeteer_calls]
+    return capitalized_calls
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(calls):
+    for call in calls:
+        if len(call) > 4:
+            return True
+    return False
 
-def find_the_cheese():
-    pass
+def find_the_cheese(snacks):
+    cheese_types = ["cheddar", "gouda", "camembert"]
+    
+    for snack in snacks:
+        if snack in cheese_types:
+            return snack
+    
+    return None
+
